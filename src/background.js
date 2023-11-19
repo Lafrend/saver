611394213b4090fd5982a272eb07c52a5a8ed85b
Lafrend <67011145+Lafrend@users.noreply.github.com>
2023-11-19 22:26:01 +0000

    jjjj

--- src/background.js
+++ src/background.js
@@ -1,21 +1,92 @@
-'use strict';
+"use strict";
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
+chrome.runtime.onInstalled.addListener(function () {
+  // Add a context menu item for selected text
+  chrome.contextMenus.create({
+    title: "Сохранить к себе",
+    id: "save-selected-text",
+    contexts: ["selection"],
+  });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+  // Add a context menu item for images
+  chrome.contextMenus.create({
+    title: "Сохранить к себе",
+    id: "save-image",
+    contexts: ["image"],
+  });
 
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
+  // Add a context menu item for clickable links
+  chrome.contextMenus.create({
+    title: "Сохранить к себе",
+    id: "save-link",
+    contexts: ["link"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId == "save-selected-text") {
+    saveItem(info);
+  } else if (info.menuItemId == "save-image") {
+    saveItem(info);
+  } else if (info.menuItemId == "save-link") {
+    saveItem(info);
   }
 });
+
+function saveItem(info) {
+  const selectedText = info.selectionText || "";
+  const imageUrl = info.srcUrl || "";
+  const linkUrl = info.linkUrl || "";
+
+  // Add the new item to your list (modify this according to your list structure)
+  const newItem = {
+    text: selectedText,
+    imageUrl: imageUrl,
+    linkUrl: linkUrl,
+    createdAt: "",
+  };
+
+  // Save the new item to local storage or wherever you store your list
+  saveNewItem(newItem);
+}
+
+function saveNewItem(item) {
+  // Add your logic to save the item to your list using chrome.storage.local
+  // Modify this function according to your storage requirements
+  console.log("Saving new item:", item);
+  chrome.storage.local.get("yourItemList", function (data) {
+    const storedList = data.yourItemList || [];
+
+    // Assign a unique index to the new item
+    item.createdAt = new Date().getTime();
+
+    // Check if the item with the same createdAt already exists
+    const existingItemIndex = storedList.findIndex(
+      (existingItem) => existingItem.createdAt === item.createdAt
+    );
+
+    if (existingItemIndex !== -1) {
+      // If the item with the same createdAt exists, remove it from the list
+      storedList.splice(existingItemIndex, 1);
+    }
+
+    // Add the new item to the beginning of the list
+    storedList.push(item);
+
+    chrome.storage.local.set({ yourItemList: storedList }, function () {
+      console.log("Item saved successfully:", item);
+    });
+  });
+}
+// Load stored data on extension open
+chrome.runtime.onInstalled.addListener(function () {
+  loadItems();
+});
+
+function loadItems() {
+  // Load stored data on extension open
+  chrome.storage.local.get("yourItemList", function (data) {
+    const storedList = data.yourItemList || [];
+    console.log("Loaded items:", storedList);
+  });
+}
--- src/popup.js
+++ src/popup.js
@@ -1,112 +1,102 @@
-'use strict';
+"use strict";
 
-import './popup.css';
+import "./popup.css";
 
-(function() {
-  // We will make use of Storage API to get and store `count` value
-  // More information on Storage API can we found at
-  // https://developer.chrome.com/extensions/storage
+console.log("This is a popup!");
 
-  // To get storage access, we have to mention it in `permissions` property of manifest.json file
-  // More information on Permissions can we found at
-  // https://developer.chrome.com/extensions/declare_permissions
-  const counterStorage = {
-    get: cb => {
-      chrome.storage.sync.get(['count'], result => {
-        cb(result.count);
-      });
-    },
-    set: (value, cb) => {
-      chrome.storage.sync.set(
-        {
-          count: value,
-        },
-        () => {
-          cb();
+const infoList = document.getElementById("infoList");
+const clearAllButton = document.getElementById("clearAllButton"); // Added clear button reference
+const createButton = document.getElementById("createButton");
+
+// Load stored data on extension open
+chrome.storage.local.get("yourItemList", function (data) {
+  const storedItemList = data.yourItemList || [];
+  displayItemList(storedItemList);
+});
+
+function displayItemList(itemListData) {
+  infoList.innerHTML = "";
+  itemListData.forEach(function (item, index) {
+    addNewItem(item, index);
+  });
+}
+
+function addNewItem(item) {
+  const li = document.createElement("li");
+  li.className = "info-item";
+
+  const span = document.createElement("span");
+  li.appendChild(span);
+
+  let itemText;
+  if (item && item.text && !item.linkUrl) {
+    itemText = item.text;
+    const textNode = document.createTextNode(itemText);
+    li.appendChild(textNode);
+  } else if (item && item.linkUrl && !item.imageUrl) {
+    const link = document.createElement("a");
+    link.href = item.linkUrl;
+    link.target = "_blank"; // Open in a new tab
+    link.textContent = item.linkUrl;
+    span.appendChild(link);
+  } else if (item && item.imageUrl) {
+    const img = document.createElement("img");
+    img.src = item.imageUrl;
+    li.appendChild(img);
+  } else {
+    itemText = "Empty.";
+  }
+
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "delete-button";
+  deleteButton.innerText = "X";
+
+  // Add click event listener to the delete button
+  deleteButton.addEventListener("click", function () {
+    deleteItem(item.id);
+  });
+
+  li.appendChild(deleteButton);
+  infoList.insertBefore(li, infoList.firstChild);
+}
+
+
+function deleteItem(index) {
+  chrome.storage.local.get("yourItemList", function (data) {
+    const storedList = data.yourItemList || [];
+
+    if (index >= 0 && index < storedList.length) {
+      // Remove the item from the list using splice
+      storedList.splice(index, 1);
+
+      // Update the local storage with the modified list
+      chrome.storage.local.set({ yourItemList: storedList }, function () {
+        console.log("Item deleted successfully at index:", index);
+
+        // Remove the deleted item from the interface
+        const itemToRemove = document.getElementById(`item-${index}`);
+        if (itemToRemove) {
+          itemToRemove.remove();
         }
-      );
-    },
-  };
-
-  function setupCounter(initialValue = 0) {
-    document.getElementById('counter').innerHTML = initialValue;
-
-    document.getElementById('incrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'INCREMENT',
       });
-    });
-
-    document.getElementById('decrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'DECREMENT',
-      });
-    });
-  }
-
-  function updateCounter({ type }) {
-    counterStorage.get(count => {
-      let newCount;
-
-      if (type === 'INCREMENT') {
-        newCount = count + 1;
-      } else if (type === 'DECREMENT') {
-        newCount = count - 1;
-      } else {
-        newCount = count;
-      }
-
-      counterStorage.set(newCount, () => {
-        document.getElementById('counter').innerHTML = newCount;
-
-        // Communicate with content script of
-        // active tab by sending a message
-        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-          const tab = tabs[0];
-
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              type: 'COUNT',
-              payload: {
-                count: newCount,
-              },
-            },
-            response => {
-              console.log('Current count value passed to contentScript file');
-            }
-          );
-        });
-      });
-    });
-  }
-
-  function restoreCounter() {
-    // Restore count value
-    counterStorage.get(count => {
-      if (typeof count === 'undefined') {
-        // Set counter value as 0
-        counterStorage.set(0, () => {
-          setupCounter(0);
-        });
-      } else {
-        setupCounter(count);
-      }
-    });
-  }
-
-  document.addEventListener('DOMContentLoaded', restoreCounter);
-
-  // Communicate with background file by sending a message
-  chrome.runtime.sendMessage(
-    {
-      type: 'GREETINGS',
-      payload: {
-        message: 'Hello, my name is Pop. I am from Popup.',
-      },
-    },
-    response => {
-      console.log(response.message);
     }
-  );
-})();
+  });
+}
+
+
+
+
+// Clear button click handler
+clearAllButton.addEventListener("click", function () {
+  clearItemList();
+});
+
+function clearItemList() {
+  infoList.innerHTML = "";
+  chrome.storage.local.remove("yourItemList");
+}
+
+// Create button click handler
+createButton.addEventListener("click", function () {
+  addNewItem();
+});
